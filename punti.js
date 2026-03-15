16c118535abb3a8a26f03fd82bda8fa6884f1a7f
luigianck219 <luigianck219@gmail.com>
2026-03-15 02:41:37 +0100

    Add files via upload

---
+++ punti.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
+const database = require('../database');
+const { buildPuntiEmbed } = require('../utils/embeds');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('punti')
+    .setDescription('⭐ Visualizza i punti di un utente')
+    .addUserOption(opt =>
+      opt.setName('utente')
+        .setDescription('Utente di cui vedere i punti (lascia vuoto per i tuoi)')
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
+
+    const targetMember = interaction.options.getMember('utente') || interaction.member;
+    const targetUser = targetMember.user;
+
+    // Ensure user exists in DB
+    await database.upsertUser(targetUser.id, targetUser.username);
+
+    const userData = await database.getUserPoints(targetUser.id);
+    const embed = buildPuntiEmbed(userData, targetMember);
+
+    await interaction.editReply({ embeds: [embed] });
+  },
+};
